d75c78e3e075137165a22bdde646b17decbd6252
F_Naru <48640767+F-Naru@users.noreply.github.com>
2025-08-28 16:34:13 +0900

    日時選択機能を修正

--- app.py
+++ app.py
@@ -15,14 +15,28 @@ def index():
         stations = [d for d in os.listdir(RECORDINGS_BASE_PATH) if os.path.isdir(os.path.join(RECORDINGS_BASE_PATH, d))]
     return render_template('index.html', stations=sorted(stations))
 
-@app.route('/api/files/<path:frequency>')
-def list_files(frequency):
-    """指定された放送局の音声ファイルリストをJSONで返す"""
+@app.route('/api/available_hours/<path:frequency>')
+def list_available_hours(frequency):
+    """指定された放送局で利用可能な「時」のリストを返す"""
+    station_path = os.path.join(RECORDINGS_BASE_PATH, frequency)
+    hours = set()
+    if os.path.exists(station_path):
+        for f in os.listdir(station_path):
+            if f.endswith('.wav') and len(f) >= 16: # yyyy-MM-ddThh-mm.wav
+                hours.add(f[:13]) # yyyy-MM-ddThh
+    return jsonify(sorted(list(hours), reverse=True))
+
+@app.route('/api/files_for_hour/<path:frequency>/<hour>')
+def list_files_for_hour(frequency, hour):
+    """指定された時間に含まれるファイルのリストを返す"""
     station_path = os.path.join(RECORDINGS_BASE_PATH, frequency)
     files = []
     if os.path.exists(station_path):
-        files = [f for f in os.listdir(station_path) if f.endswith('.wav')]
-    return jsonify(sorted(files, reverse=True))
+        for f in os.listdir(station_path):
+            if f.startswith(hour) and f.endswith('.wav'):
+                files.append(f)
+    return jsonify(sorted(files))
+
 
 @app.route('/audio/<path:frequency>/<path:filename>')
 def serve_audio(frequency, filename):
